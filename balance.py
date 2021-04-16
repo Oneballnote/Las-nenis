0d97ff4fe06ed8994d2ec29766f099cfb3bdaa3f
Samuelrodriguezp <82631441+Samuelrodriguezp@users.noreply.github.com>
2021-04-15 23:54:08 -0500

    Update balance.py

--- balance.py
+++ balance.py
@@ -6,4 +6,6 @@ def ventana_balance():
     main_window = tkinter.Tk()
     
     balance = tkinter.Label(main_window, text = 'Balance')
+    boton_venta= tkinter.Label(mainwindow, text = 'Registrar Venta',bg = '#6990D8')
+    boton_carga= tkinter.Label(mainwindow,text = 'Registrar Carga',bg = '#6990D8')
     tkinter.mainloop()

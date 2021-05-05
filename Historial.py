553989745bc6a50f7607d4d39e119457cf5aebe3
Samuelrodriguezp <82631441+Samuelrodriguezp@users.noreply.github.com>
2021-05-05 14:23:28 -0500

    Update Historial.py

--- Historial.py
+++ Historial.py
@@ -1,4 +1,5 @@
 import tkinter
+import pandas as pd
 from datetime import datetime
 dt = datetime.now()
 
@@ -15,18 +16,41 @@ def Ventana_Historial():
     Domingo = tkinter.Label(main_window, text = 'Domingo').grid(row = 9,column = 1)
     
     ########Para  abrir historial########
+#Si click en botón bolis entonces:
 histbolis = pd.read_csv("registro_bolis.csv")
-histbolis
-#Para solo ventas / carga
-soloventas = histbolis[histbolis["Acción"]=="Venta"]
-soloventas
-#convertir fecha en cadena a un datetime
-from datetime import datetime 
-fecha = input("Inserte fecha a buscar en formato dd/mm/aaaa")
-fecha_object = datetime.strptime(fecha, "%d/%m/%Y")
-"gay",fecha_object
+print(histbolis)
+#Si botón en solo ventas entonces:
+soloventasbolis = histbolis[histbolis["Acción"]=="Venta"]
+print(soloventasbolis)
+#Sinosi botón en solo carga entonces:
+solocargabolis = histbolis[histbolis["Acción]=="Carga"]
+print(solocargabolis)
+                      
+#si click en botón botanas entonces:
+histbotanas = pd.read_csv("registro_botanas.csv")
+print(histbotanas)
+#Si botón en solo venta entonces:
+soloventabotanas = histbotanas[histbotanas["Acción"]=="Venta"]
+print(soloventabotanas)
+#Sinosi botón en solo carga entonces:
+solocargabotanas = histbotanas[histbotanas["Acción"]=='Carga']
+print(solocargabotanas)
+                                        
+    #Para buscar un día en específico 
+    #Para buscar un día en específico 
+histbolis = pd.read_csv("registro_bolis.csv")
+print(type(histbolis.iloc[0,0]))
+histbolis.index = histbolis["Fecha"]
+histbolis.drop("Fecha", axis=1, inplace=True)
 
-    
+busqueda = input("Ingrese día a buscar en formato dd/mm/aaaa: ")
+try:
+    histbolisbusquedadia = histbolis.loc[busqueda]    
+except:
+    print("No hay registro del día ingresado")
+
+histbolis.reset_index(inplace=True)
+
     salir = tkinter.Button(main_window,text = 'Volver', command = main_window.destroy)
     
     tkinter.mainloop()

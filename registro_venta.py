eea93c189a8b0aba8d2e3511f0bd8eca94743c0e
Samuelrodriguezp <82631441+Samuelrodriguezp@users.noreply.github.com>
2021-05-06 19:10:59 -0500

    Delete registro_venta.py

--- registro_venta.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-from datetime import datetime
-dt = datetime.now()
-
-def registro_venta():
-    main_window = tkinter.Tk()
-    
-    balance = tkinter.Label(main_window, text = 'Balance')
-    tkinter.mainloop()
